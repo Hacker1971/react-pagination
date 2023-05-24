5fefd752406651d90699f7fb93e951320d83ff85
tanvir-islam <2020tanvir19711@gmail.com>
2023-05-24 20:58:24 +0600

    all

---
+++ src/componets/utils/paginate.js
@@ -0,0 +1,12 @@
+export const pageinate = (data = [], perPage = 10) => {
+    const page = Math.ceil(data.length / perPage);
+  
+    const newData = Array.from({ length: page }, (_, index) => {
+      const start = index * perPage;
+  
+      return data.slice(start, start + perPage);
+    });
+  
+    return newData;
+  };
+  
